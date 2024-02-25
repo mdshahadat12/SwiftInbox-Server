8960661488e17c473e0f853a49607a8705c10fdd
PallabKumarS <pallabkumar26@gmail.com>
2024-02-25 17:45:16 +0600

    done with backend for read and unread feature

--- src/routes/mail/index.js
+++ src/routes/mail/index.js
@@ -3,11 +3,13 @@ const createNewMail = require("../../api/mail/createNewMail");
 const getNewMail = require("../../api/mail/getNewMail");
 const getDomains = require("../../api/mail/getDomains");
 const updateMail = require("../../api/mail/updateMail");
+const notifyMail = require("../../api/mail/notifyMail");
 var router = express.Router();
 
 router.get("/new", createNewMail);
 router.get("/get-mail", getNewMail);
 router.get("/get-domains", getDomains);
 router.put("/update-mail/:id", updateMail);
+router.put("/notify-mail/:id", notifyMail);
 
 module.exports = router;
