fb37d5333e6d500c02c3dbbbe99b645cd97514af
PallabKumarS <pallabkumar26@gmail.com>
2024-03-08 22:10:43 +0600

    fixed

--- src/api/Payment/PaymentIntent.js
+++ src/api/Payment/PaymentIntent.js
@@ -1,23 +1,22 @@
-const stripe = require("stripe")(process.env.PAYMENT_SK)
+const stripe = require("stripe")(process.env.PAYMENT_SK);
 
-
-const Paymentintent = async (req, res , next) => {
-    try{
+const Paymentintent = async (req, res, next) => {
+  try {
     const { price } = req.body;
     const amount = parseInt(price * 100);
-    console.log(amount, 'amount')
-    if(amount<1){
-      return
+    console.log(amount, "amount");
+    if (amount < 1) {
+      return;
     }
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
-      currency: 'usd',
-      payment_method_types: ['card']
-    })
-    res.send(paymentIntent.client_secret)
-    } catch (err) {
-        next(err);
-      }
-  };
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
+    res.send(paymentIntent.client_secret);
+  } catch (err) {
+    next(err);
+  }
+};
 
-  module.exports = Paymentintent;
+module.exports = Paymentintent;
